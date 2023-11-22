5eb7bb0b62f4dfd89bd9d67c55d68c090973d177
alex2209alex <alex.pavel2002@gmail.com>
2023-11-22 21:44:41 +0200

    Configurare initiala proiect backend + frontend

---
+++ cod-sursa/backend/src/test/java/ro/unibuc/fmi/ge/GeApplicationTests.java
@@ -0,0 +1,13 @@
+package ro.unibuc.fmi.ge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
